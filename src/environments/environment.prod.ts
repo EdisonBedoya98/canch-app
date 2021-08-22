d7ed074a4214a8b9b1f840ff591f78d2ce81f1c3
Edison <edison.bedoyag@udea.edu.co>
2021-08-22 11:16:43 -0500

    is included the new page to validate the email

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDvib5cts2EpIOooCm645XI-DH1hrW67lw",
+    authDomain: "canchapp-afa8a.firebaseapp.com",
+    projectId: "canchapp-afa8a",
+    storageBucket: "canchapp-afa8a.appspot.com",
+    messagingSenderId: "282065120891",
+    appId: "1:282065120891:web:7284fd47430f0bc9ba7cde"
+  }
 };
